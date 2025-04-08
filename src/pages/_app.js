46193b72d43a4a78f87c8816273e40beb81d1c11
Rakan Alshubat <rakan.alshubat@gmail.com>
2025-04-08 12:14:43 -0700

    fixing extra code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,9 +5,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../styles/theme'
-import Navbar from '@/components/NavBar';
 import createEmotionCache from '../styles/createEmotionCache';
-import Background from 'src/media/Background.jpg'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -17,13 +15,12 @@ export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
-      <Navbar />
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div style={{ backgroundImage: `url(${Background.src})`, backgroundSize: 'cover', backgroundSize: 'f'}}>.
+        <div>
           <Component {...pageProps} />.
         </div>
       </ThemeProvider>
