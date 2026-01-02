9d38b080e75e75a8227580d84cc51aaf4e66e22e
Rakan Alshubat <rakan.alshubat@gmail.com>
2026-01-01 16:14:03 -0800

    testing the secrets

--- src/pages/Player.js
+++ src/pages/Player.js
@@ -1,7 +1,9 @@
 import PlayerPage from "@/files/PlayerPage";
+import { secret } from "@aws-amplify/backend"
 import { NextSeo } from 'next-seo';
 
 export default function Player(){
+    console.log(secret('SENDGRID_FROM_EMAIL'));
     return (
         <>
             <NextSeo title="Player — Drag League" description="Your account, leagues, and submissions on Drag League." />
