5b198d1b319861cf09851e057c5795fce722141a
Rakan Alshubat <rakan.alshubat@gmail.com>
2026-01-02 11:34:06 -0800

    done after this

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    env: {
+        SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+        SENDGRID_FROM_EMAIL: process.env.SENDGRID_FROM_EMAIL,
+        SENDGRID_FROM_NAME: process.env.SENDGRID_FROM_NAME,
+        SENDGRID_REPLY_TO_EMAIL: process.env.SENDGRID_REPLY_TO_EMAIL,
+    }
 }
 
 module.exports = nextConfig
