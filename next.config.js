db47651d84574d4c75702d1f372f657491f1975e
Rakan Alshubat <rakan.alshubat@gmail.com>
2026-01-02 16:39:49 -0800

    undo

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // Required for API routes to work on Amplify
-    output: 'standalone',
     env: {
         SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
         SENDGRID_FROM_EMAIL: process.env.SENDGRID_FROM_EMAIL,
