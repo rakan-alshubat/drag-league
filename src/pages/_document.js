ded6f6fbffd81cd568cf7d5fe8eb59ecd4cee47a
Rakan Alshubat <rakan.alshubat@gmail.com>
2026-01-13 14:54:03 -0800

    come on

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,9 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon-v2.png" />
 
                 <meta name="apple-mobile-web-app-capable" content="yes" />
-                <meta name="apple-mobile-web-app-status-bar-style" content="default" />
                 <meta name="apple-mobile-web-app-title" content="Drag League" />
 
             </Head>
