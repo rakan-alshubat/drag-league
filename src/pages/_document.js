aa8e90033e675234f1ab9da0c4e660bc1897c7fc
Rakan Alshubat <rakan.alshubat@gmail.com>
2026-01-13 14:02:13 -0800

    lint the ugh

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="default" />
-<meta name="apple-mobile-web-app-title" content="Drag League" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+                <meta name="apple-mobile-web-app-title" content="Drag League" />
 
             </Head>
             <body>
