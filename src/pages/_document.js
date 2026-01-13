5d344e726da38cf799208a9c2768ae04fa7c9f6c
Rakan Alshubat <rakan.alshubat@gmail.com>
2026-01-13 10:15:35 -0800

    lets see

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,12 @@ export default function Document() {
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
                 <meta name="apple-mobile-web-app-title" content="Drag League" />
-                <link rel="apple-touch-icon" href="/favicon.svg" />
+                {/* Recommended: provide PNG apple-touch-icon (180x180) for iOS */}
+                <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+                {/* Fallback favicons for various platforms */}
+                <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192.png" />
+                <link rel="icon" type="image/png" sizes="512x512" href="/icons/icon-512.png" />
+                <meta name="msapplication-TileImage" content="/icons/icon-192.png" />
                 
                 {/* Mobile Optimization */}
                 <meta name="mobile-web-app-capable" content="yes" />
