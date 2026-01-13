2fe123c911708ff2755cfb8c170e3b645204d315
Rakan Alshubat <rakan.alshubat@gmail.com>
2026-01-13 15:03:25 -0800

    putting everything back

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,9 +4,10 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel="apple-touch-icon" href="/apple-touch-icon-v2.png" />
-
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <meta name="theme-color" content="#FF1493" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="default" />
                 <meta name="apple-mobile-web-app-title" content="Drag League" />
 
             </Head>
